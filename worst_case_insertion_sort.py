2963f5d06a8f244e02f25976daed79b6d181c178
Felix Schmid <felix.schmid@hotmail.de>
2018-09-22 11:37:11 +0200

    worst case

---
+++ worst_case_insertion_sort.py
@@ -0,0 +1,52 @@
+
+# coding: utf-8
+
+# In[34]:
+
+
+# Imports:
+
+
+import random
+import time
+import matplotlib.pyplot as plt
+
+
+# Functions:
+
+
+def create_cards(num_cards):
+    cards = list(range(num_cards, 0, -1))
+    #random.shuffle(cards)
+    return(cards)
+
+def sorting(cards):
+    for j in range(1, len(cards)):
+        key = cards[j]
+        i = j - 1
+        while i >= 0 and cards[i] > key:
+            cards[i+1] = cards[i]
+            i -= 1
+        cards[i+1]=key
+    return(cards)
+
+def worst_case_time(set_cards):
+    start = time.time()
+    sorting(cards)
+    measure_time = time.time()-start
+    print("Worst case time: " + "{0:.9f}".format(measure_time) + " seconds")
+    return measure_time
+
+
+
+# Execution:
+
+
+worst_time_in_sec = []
+number_of_cards = [10, 100, 1000, 10000]
+
+for number_cards in range(4):
+    cards = create_cards(number_of_cards[number_cards])
+    time_in_sec = worst_case_time(cards)
+    worst_time_in_sec.append(time_in_sec)
+

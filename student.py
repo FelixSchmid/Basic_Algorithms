bd266386e03ab50584175e800e5b5177e4f1f17c
Felix Schmid <felix.schmid@hotmail.de>
2018-10-11 11:30:04 +0200

    created new class

---
+++ student.py
@@ -0,0 +1,9 @@
+class Student:
+    
+    def __init__(self, firstname, lastname):
+        self.firstname = firstname
+        self.lastname  = lastname
+        self.birthday = birthday
+        self.grade = grade   
+
+felix = Student('Felix', 'Schmid', '21.10.93', '1')
